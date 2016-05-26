8f8080bf6f7e010572b433ca06a6908d9fb35351
Xiaoyu Lei <xlei@cimpress.com>
2016-05-26 11:04:35 -0400

    Update database url

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
 module.exports = {
-  url: 'mongodb://chinateam:china@ds023902.mlab.com:23902/cimpress_china',
+  // swap to the following url when developing in local
+  // url: 'mongodb://chinateam:china@ds023902.mlab.com:23902/cimpress_china', 
+  url: 'mongodb://order_service:sa@dds-uf6577b058aaac842.mongodb.rds.aliyuncs.com:3717,dds-uf6577b058aaac841.mongodb.rds.aliyuncs.com:3717/Test_Orders?replicaSet=mgset-1192893',
 };
